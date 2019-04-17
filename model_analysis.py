fix small bugs, change seeded sampling to use fixed variance (0.1), issue with number of property predictor neurons, track epoch running time

model_analysis.py
@@ -140,7 +140,7 @@ def property_distributions(data_test, props_test, num_seeds, num_decodings, mode
             mu, logvar = model.get_moments(seq)
             for dec_itr in range(num_decodings):
                 # c_output = output(mu,logvar)
-                s = model.decode_from_moments(mu, logvar, beam_width)
+                s = model.decode_from_moments(mu, np.log(0.1)*np.ones_like(logvar), beam_width)
                 s = np.array(s)
                 if s.ndim > 1: s = s[:, 0]
                 success = False  # Track whether we produced a valid molecule from this seed

molecules/transformer.py
@@ -349,7 +349,7 @@ class AvgLatent2():
 class AvgLatent4():
     def __init__(self, d_model, latent_dim, heads=4):
         d_kqv = int(np.ceil(d_model / heads))
-        self.attns = [KQV_Attn(d_model, d_kqv, activation='relu') for _ in range(heads)]
+        self.attns = [KQV_Attn(d_model, d_kqv, activation='linear') for _ in range(heads)]
         self.after_avg = Dense(latent_dim, input_shape=(d_kqv * heads,), activation='relu')
         self.mean_layer = Dense(latent_dim, input_shape=(latent_dim,), name='mean_layer')
         self.logvar_layer = Dense(latent_dim, input_shape=(latent_dim,), name='logvar_layer')
@@ -1175,13 +1175,14 @@ class InterimDecoder5():
 
 
 class FalseEmbeddingsTD():
-    def __init__(self, d_emb, residual=True, layers=0):
+    def __init__(self, d_emb, d_latent=None, residual=True, layers=1):
         '''
         Given a 1D vector, attempts to create 'false' embeddings to
         go from latent space
         :param d_emb: dimensionality of false embeddings
         '''
-
+        self.l1 = Dense(d_latent, input_shape=(d_latent,), activation='relu')
+        self.l2 = Dense(d_latent, input_shape=(d_latent,))
         self.init_layer = TimeDistributed(Dense(d_emb, input_shape=(1,)))
         self.deep_layers = [TimeDistributed(Dense(d_emb, activation='relu', input_shape=(d_emb,))) for _ in
                             range(layers)]
@@ -1198,7 +1199,8 @@ class FalseEmbeddingsTD():
         :param z: Input with dimensionality [batch_size, d] or [batch_size, d, 1]
         :return: Falsely embedded output with dimensionality [batch_size, d, d_emb]
         '''
-
+        z = self.l1(z)
+        z = self.l2(z)
         if z.shape.ndims == 2:
             z = Lambda(lambda x: K.expand_dims(x, axis=2))(z)
 
@@ -1209,12 +1211,9 @@ class FalseEmbeddingsTD():
             if self.residual:
                 z_w = self.deep_res_layers[i](layer(z))
                 z = Add()([z, z_w])
-                z = TimeDistributed(BatchNormalization)(z)
-                z = ReLU()(z)
                 # z = z + layer(z)
             else:
                 z = layer(z)
-                z = TimeDistributed(BatchNormalization)(z)
 
         return self.final_lin(z)
 

train_preset.py
@@ -258,8 +258,8 @@ def main():
                                                              params["pp_weight"])
     props_train, props_test, prop_labels = utils.load_properties(args.data)
 
-    num_seeds = 1000
-    num_decodings = 3
+    num_seeds = 500
+    num_decodings = 10
     num_prior_samples = 1000
     with supress_stderr():
         seed_output = property_distributions(data_test, props_test,
@@ -337,6 +337,7 @@ def createResultsFile(models_dir):
         d = ["Model name",
              "Epochs",
              "Val_acc",
+             "Epoch_time",
              "S: Num seeds",
              "S: Num decodings"]
         append_titles(d)
@@ -359,7 +360,7 @@ def saveResults(params, val_acc, seeded_output, prior_output, num_seeds, num_dec
             d.append(np.round(np.mean(output["gen_props"][:, i]), 3))
             d.append(np.round(np.std(output["gen_props"][:, i]), 3))
 
-    d = [params["model"], params["epochs"], val_acc, num_seeds, num_decodings]
+    d = [params["model"], params["epochs"], val_acc, params["epoch_time"], num_seeds, num_decodings]
     append_output(seeded_output, d)
     d.append(num_prior_samples)
     append_output(prior_output, d)

utils.py
@@ -2,7 +2,7 @@
 import os
 import pickle
 from os.path import dirname
-
+import time
 import h5py
 import numpy as np
 from keras import backend as K
@@ -48,7 +48,12 @@ class epoch_track(Callback):
         else:
             self.csv_filename = None
 
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time = time.time()
+
     def on_epoch_end(self, epoch, logs={}):
+        self.epoch_time = time.time() - self.epoch_time
+        self._params["epoch_time"] = (self._params["epoch_time"]*epoch + self.epoch_time)/(epoch+1)
         self._params["current_epoch"] += 1
         self._params.save(self._filename)
         epoch += 1
@@ -70,7 +75,7 @@ class epoch_track(Callback):
             arr[self.rownum, num_params + 2 * epoch - 1] = logs["val_acc"]
             # save csv
             np.savetxt(self.csv_filename, arr, delimiter=",", fmt='%s')
-
+        self.epoch_time = time.time()
         return
 
     def on_train_end(self, logs={}):
@@ -385,7 +390,8 @@ class AttnParams:
             "ID_d_v": None,
             "ID_layers": None,
             "ID_width": None,
-            "num_params": None
+            "num_params": None,
+            "epoch_time": 0,
         }
 
     def __getitem__(self, param):